a96771f6efc3555e32b8f43d832bf268d27d08c2
AbdukarimPro <abdukarimqarshiyev1@gmail.com>
2022-03-22 13:00:02 +0500

    music player

---
+++ js/music-list.js
@@ -0,0 +1,104 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Давай не Болей",
+    artist: "Adam",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "Yolg`onchi Yor",
+    artist: "Akbar Ravshanov",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "Как Не Любить",
+    artist: "Andro",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Osmondan",
+    artist: "Asror",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "Sarmaya Geldim",
+    artist: "Ayten Rasul",
+    img: "music-5",
+    src: "music-5",
+  },
+  {
+    name: "Ты Читаешь Библию, А Я Коран",
+    artist: "Babek Mamedrzaev",
+    img: "music-6",
+    src: "music-6",
+  },
+  {
+    name: "Bo'larkan",
+    artist: "Doston Ergashev",
+    img: "music-7",
+    src: "music-7",
+  },
+  {
+    name: "Шейхами",
+    artist: "Gazan",
+    img: "music-8",
+    src: "music-8",
+  },
+  {
+    name: "Ko'zmunchog'im",
+    artist: "Doston Ergashev",
+    img: "music-9",
+    src: "music-9",
+  },
+  {
+    name: "Yomg`ir Yog`aver",
+    artist: "Oybek & Nigora",
+    img: "music-10",
+    src: "music-10",
+  },
+  {
+    name: "Onamni Asra Robbim",
+    artist: "Uzmir",
+    img: "music-11",
+    src: "music-11",
+  },
+  {
+    name: "Kimsalar",
+    artist: "Jaloliddin Ahmadaliyev & Alisher Nematov",
+    img: "music-12",
+    src: "music-12",
+  },
+  {
+    name: "Yor Yor(Orginal MIX)",
+    artist: "King",
+    img: "music-13",
+    src: "music-13",
+  },
+  {
+    name: "Богатый",
+    artist: "Tural Everest (feat. Руслан Добрый)",
+    img: "music-14",
+    src: "music-14",
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
